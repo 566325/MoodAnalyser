2be3339f4cc753fa4fed89bab0f83218b16de35b
566325 <nethisireesha288@gmail.com>
2023-09-29 23:14:22 +0530

    [Sireesha]:Added Simple Mood Analyser Problem

---
+++ Mood.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class Mood
+    {
+        public string messege;
+        public Mood()
+        {
+
+        }
+        public Mood(string messege)
+        {
+            this.messege = messege;
+        }
+        public string MoodAnalyse()
+        {
+            if (messege.ToUpper().Contains("SAD"))
+            {
+                return "Sad Mood";
+            }
+            else
+            {
+                return "Happy Mood";
+            }
+        }
+    }
+}
